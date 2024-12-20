64cd9aac09cc8d9e4b5a63fbf4fd18e11ab09fc0
Gan Chin Yao <chinyao95@gmail.com>
2024-12-20 23:14:41 +0800

    feat: add home page hero text

--- src/app/components/home/HomePageComponent.tsx
+++ src/app/components/home/HomePageComponent.tsx
@@ -1,9 +1,16 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Text, VStack } from '@chakra-ui/react'
+import { InvitationButton } from '@/app/components/invitation/InvitationButton'
 
 export const HomePageComponent = () => {
   return (
-    <Box>
-      <Text>Home</Text>
-    </Box>
+    <VStack gap={4} justifyContent="center" textAlign="center" h={'100%'}>
+      <Text textStyle={'5xl'}>
+        A better way
+        <br />
+        to enjoy every day.
+      </Text>
+      <Text>Experience the Future of Convenience.</Text>
+      <InvitationButton />
+    </VStack>
   )
 }
---
+++ src/app/components/invitation/InvitationButton.tsx
@@ -0,0 +1,14 @@
+'use client'
+import { Button } from '@chakra-ui/react'
+
+export const InvitationButton = () => {
+  return (
+    <Button
+      onClick={() => {
+        //TODO: Implement the invitation request
+      }}
+    >
+      Request an invitation
+    </Button>
+  )
+}
